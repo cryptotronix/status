2b944e021f2f03afdd39ff6893d96c975793e88e
Josh Datko <jbd@cryptotronix.com>
2014-09-28 19:05:03 -0600

    Initial test.

---
+++ status.py
@@ -0,0 +1,32 @@
+from google.appengine.api import users
+
+import webapp2
+import json
+import urllib2
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        # Checks for active Google account session
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('Hello, ' + user.nickname())
+            self.response.write(self.get_num_cryptocapes())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+    def get_num_cryptocapes(self):
+        rsp = urllib2.urlopen('https://www.sparkfun.com/products/12773.json')
+        html = rsp.read()
+        j = json.loads(html)
+
+        return j['quantity']
+
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
